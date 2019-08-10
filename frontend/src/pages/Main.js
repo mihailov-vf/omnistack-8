ca6065058da37032b1fc3b5df609bc5ec2a41e1d
Mihailov Vasilievic Filho <mihailov.vf@gmail.com>
2019-08-10 14:40:15 -0300

    Estilização da página principal

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,9 +1,61 @@
 import React from 'react';
+import './Main.css';
 
-export default function Main() {
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+
+export default function Main({ match }) {
     return (
-        <div>
-            <h1>Hello World!</h1>
+        <div className="main-container">
+            <img src={logo} alt="Tindev" />
+            <ul>
+                <li>
+                    <img src="https://avatars3.githubusercontent.com/u/4248081?s=460&v=4" alt="Dev avatar" />
+                    <footer>
+                        <strong>Nome</strong>
+                        <p>Bio asdasdasdasd dasd asd asd asd asd asdasdadasdasdasd asdasdasdasd asdasdasdasdasda asdasdasdasd asdasdasdasda asdasdasdasdasd asdasdasds</p>
+                    </footer>
+                    <div className="buttons">
+                        <button>
+                            <img src={like} alt="Like" />
+                        </button>
+                        <button>
+                            <img src={dislike} alt="Dislike" />
+                        </button>
+                    </div>
+                </li>
+                <li>
+                    <img src="https://avatars3.githubusercontent.com/u/4248081?s=460&v=4" alt="Dev avatar" />
+                    <footer>
+                        <strong>Nome</strong>
+                        <p>Bio</p>
+                    </footer>
+                    <div className="buttons">
+                        <button>
+                            <img src={like} alt="Like" />
+                        </button>
+                        <button>
+                            <img src={dislike} alt="Dislike" />
+                        </button>
+                    </div>
+                </li>
+                <li>
+                    <img src="https://avatars3.githubusercontent.com/u/4248081?s=460&v=4" alt="Dev avatar" />
+                    <footer>
+                        <strong>Nome</strong>
+                        <p>Bio</p>
+                    </footer>
+                    <div className="buttons">
+                        <button>
+                            <img src={like} alt="Like" />
+                        </button>
+                        <button>
+                            <img src={dislike} alt="Dislike" />
+                        </button>
+                    </div>
+                </li>
+            </ul>
         </div>
     );
 }
