276addf6f32846ce5402d3c653306638dd44e587
Mihailov Vasilievic Filho <mihailov.vf@gmail.com>
2019-08-11 14:24:50 -0300

    Realtime match no backend

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -12,7 +12,16 @@ module.exports = {
             return res.status(400).json({error: 'Dev does not exists'});
         }
         if(targetDev.likes.includes(loggedDev._id)) {
-            console.log('Deu Match');
+            const loggedSocket = req.connectedUsers[user];
+            const targetSocket = req.connectedUsers[devId];
+
+            if(loggedSocket) {
+                req.io.to(loggedSocket).emit('match', targetDev);
+            }
+
+            if(targetSocket) {
+                req.io.to(targetSocket).emit('match', loggedDev);
+            }
         }
 
         loggedDev.likes.push(targetDev._id);
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,13 +3,30 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+
+    connectedUsers[user] = socket.id;
+});
 
 mongoose.connect('mongodb://omnistack:omnistack-8@ds261277.mlab.com:61277/omnistack-8', {
     useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
 server.listen(3333);
