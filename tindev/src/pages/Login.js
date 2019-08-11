ac89c825204fd335b25b589881ced542346e0963
Mihailov Vasilievic Filho <mihailov.vf@gmail.com>
2019-08-11 00:35:41 -0300

    Comportamento de login com api

--- tindev/src/pages/Login.js
+++ tindev/src/pages/Login.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 import {
   KeyboardAvoidingView,
   Platform,
@@ -9,14 +10,30 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
+import api from '../services/api';
+
 import logo from '../assets/logo.png';
 
 export default function Login({ navigation }) {
   const [user, setUser] = useState('');
-  function handleLogin() {
-    console.log(user);
 
-    navigation.navigate('Main');
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user => {
+      if (user) {
+        navigation.navigate('Main', { user });
+      }
+    });
+  }, [navigation]);
+
+  async function handleLogin() {
+    const response = await api.post('/devs', {
+      username: user,
+    });
+
+    const { _id } = response.data;
+    await AsyncStorage.setItem('user', _id);
+
+    navigation.navigate('Main', { _id });
   }
 
   return (
