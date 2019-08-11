7cffa2a70238ab6e93f39b15c5232e1ccc7c8889
Mihailov Vasilievic Filho <mihailov.vf@gmail.com>
2019-08-11 14:25:35 -0300

    Realtime match no frontend

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import './Main.css';
 
 import api from '../services/api';
@@ -6,9 +7,11 @@ import api from '../services/api';
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
+import itsamatch from '../assets/itsamatch.png';
 
 export default function Main({ match }) {
     const [users, setUsers] = useState([]);
+    const [matchDev, setMatchDev] = useState(null);
 
     useEffect(() => {
         async function loadDevs() {
@@ -21,6 +24,18 @@ export default function Main({ match }) {
             setUsers(response.data);
         };
         loadDevs();
+    }, [match.params.id]);
+
+    useEffect(() => {
+        const socket = io('http://localhost:3333', {
+            query: {
+                user: match.params.id
+            }
+        });
+
+        socket.on('match', dev => {
+            setMatchDev(dev);
+        })
     }, [match.params.id]);
 
     function removeUser(id) {
@@ -76,6 +91,15 @@ export default function Main({ match }) {
                 </ul>
             ) : (
                 <div className="empty">Acabou! :(</div>
+            )}
+            {matchDev && (
+                <div className="match-container">
+                    <img src={itsamatch} alt="Deu match!" />
+                    <img className="avatar" src={matchDev.avatar} alt={matchDev.name} />
+                    <strong>{matchDev.name}</strong>
+                    <p>{matchDev.bio}</p>
+                    <button onClick={() => {setMatchDev(null)}}>Fechar</button>
+                </div>
             )}
         </div>
     );
